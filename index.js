15bf60f1171639b456cc2c096ac1feb8b8c80eb8
Kiana Long <kianajade42.kl@gmail.com>
2021-04-12 19:37:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+
+ var wakeDog function(dogName, dogBreed){console.log(`Wake ${dogName} the ${dogBreed}`);}
+ var leashDog function(dogName, dogBreed){console.log(`Leash ${dogName} the ${dogBreed}`);}
+var walkToPark function(dogName,dogBreed){console.log(` Walk ${dogName} the ${dogBreed}`);}
+var  throwFrisbee function(dogName,dogBreed){console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);}
+var walkHome function(dogName,dogBreed){console.log(`Walk home with ${dogName} the ${dogBreed}`);}
+var unleashDog function(dogName,dogBreed){console.log(`Unleash ${dogName} the ${dogBreed}`);}
+ 
+const routine= [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
+function exerciseDog(dogName, dogBreed){return routine.map}
