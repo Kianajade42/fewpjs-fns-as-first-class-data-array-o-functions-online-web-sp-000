b8b412ba8b91f60d6d1b2c801cde34bfe75ba8e6
Kiana Long <kianajade42.kl@gmail.com>
2021-04-12 19:54:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
   let wakeDog =function(dogName, dogBreed){console.log(`Wake ${dogName} the ${dogBreed}`);}
- var leashDog =function(dogName, dogBreed){console.log(`Leash ${dogName} the ${dogBreed}`);}
- var walkToPark= function(dogName,dogBreed){console.log(` Walk ${dogName} the ${dogBreed}`);}
- var  throwFrisbee= function(dogName,dogBreed){console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);}
- var walkHome =function(dogName,dogBreed){console.log(`Walk home with ${dogName} the ${dogBreed}`);}
- var unleashDog= function(dogName,dogBreed){console.log(`Unleash ${dogName} the ${dogBreed}`);}
+ let leashDog =function(dogName, dogBreed){console.log(`Leash ${dogName} the ${dogBreed}`);}
+ let walkToPark= function(dogName,dogBreed){console.log(` Walk ${dogName} the ${dogBreed}`);}
+   let throwFrisbee= function(dogName,dogBreed){console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);}
+  let walkHome =function(dogName,dogBreed){console.log(`Walk home with ${dogName} the ${dogBreed}`);}
+  let unleashDog= function(dogName,dogBreed){console.log(`Unleash ${dogName} the ${dogBreed}`);}
 
 const routine= [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
 function exerciseDog(dogName, dogBreed){return routine.map(fn=>fn(dogName,dogBreed))}
