56ce163cd5ebf2d5f7d0a3219feb27d160ffff64
Kiana Long <kianajade42.kl@gmail.com>
2021-04-12 20:17:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ var unleashDog = function(dogName="Byron", dogBreed="poodle") {
 
 const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
 
-function exerciseDog(dog, breed) {
-  return routine.map(fn => fn(dog, breed))
+function exerciseDog(dogName, dogBreed) {
+  return routine.map(fn => fn(dogName, dogBreed))
 }
