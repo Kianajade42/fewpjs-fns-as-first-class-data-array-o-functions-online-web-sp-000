458a661909e26301d9232303f4376aa1d334ed99
Kiana Long <kianajade42.kl@gmail.com>
2021-04-12 20:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ let leashDog = function(dogName="a", dogBreed="b")
  {console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`}
 
-let walkToPark = function(dogName="a", dogBreed="b") 
+let walkToPark = function(dogName="a", dogBreed="b")
 {console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
   return `Walk to the park with ${dogName} the ${dogBreed}`}
 
-let throwFrisbee = function(dogName="a", dogBreed="b") 
+let throwFrisbee = function(dogName="a", dogBreed="b")
 {console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
   return `Throw the frisbee for ${dogName} the ${dogBreed}`}
 
-let walkHome = function(dogName="a", dogBreed="b") 
+let walkHome = function(dogName="a", dogBreed="b")
 {console.log(`Walk home with ${dogName} the ${dogBreed}`);
   return `Walk home with ${dogName} the ${dogBreed}`}
 
